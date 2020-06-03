e348bf0190d802bfd6901d5b801ab29e2c687e51
ali saee <saeeali1979@gmail.com>
2020-06-03 17:06:34 +0200

    first commit

---
+++ function_exercises.js
@@ -0,0 +1,46 @@
+// Exercise 1
+
+// function checkAge(age) {
+//   if (age > 18) {
+//     return true;
+//   } else {
+//     return confirm('Do you have your parents permission to access this page?');
+//   }
+// }
+
+// age => (age > 18) ? 'true' : 'confirm'
+
+Exercise 2
+
+pow(3, 2) = 3 * 3 = 9
+pow(3, 3) = 3 * 3 * 3 = 27
+pow(1, 100) = 1 * 1 * ...* 1 = 1
+
+// Exercise 3
+
+// function ask(question, yes, no) {
+//   if (confirm(question)) yes()
+//   else no()
+// }
+
+// ask(
+//   "Do you agree?",
+//   () => { alert("You agreed.") },
+//   () => { alert("You canceled the execution.") }
+//   );
+
+Exercise 4
+
+let calculator = {
+  // ... your code ...
+};
+
+calculator.read();
+alert( calculator.sum() );
+alert( calculator.mul() );
+
+// Exercise 5: Function min(a, b)
+
+// function min(a, b) {
+//   return a < b ? a : b;
+// }
